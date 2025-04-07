5db48cbc30bc8b95c83fc6cc240501ca43f69eb4
dmkmsds <junjietan34@gmail.com>
2025-04-07 22:32:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ from pypinyin import lazy_pinyin, Style
 import jieba
 from transformers import AutoTokenizer, AutoModel
 
-torch.classes.__path__ = []
+#torch.classes.__path__ = []
 
 st.write("Checking internet...")
 try:
